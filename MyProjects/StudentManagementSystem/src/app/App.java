95a7d5d376b59ae4b962bbf0e1bde580b8b8e758
akshayoffice244 <akshayoffice244@gmail.com>
2026-03-30 16:44:54 +0530

    added exception handling to student management system

--- MyProjects/StudentManagementSystem/src/app/App.java
+++ MyProjects/StudentManagementSystem/src/app/App.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import handler.StudentManager;
@@ -7,65 +8,77 @@ import model.Student;
 
 public class App {
 	public static void main(String[] args) {
-		
-			Scanner sc = new Scanner(System.in);
-			StudentManager manager = new StudentManager();
-			int exit = 1;
-			while(exit == 1) {
-				
-				 System.out.println("\n1. Add Student");
-		         System.out.println("2. View All");
-		         System.out.println("3. Search Student");
-		         System.out.println("4. Remove Student");
-		         System.out.println("5. Exit");
 
-		         int choice = sc.nextInt();
-		         switch(choice) {
-		         case 1:
-	                    System.out.print("Enter ID: ");
-	                    int id = sc.nextInt();
-	                    sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+		StudentManager manager = new StudentManager();
+		int exit = 1;
+		while (exit == 1) {
 
-	                    System.out.print("Enter Name: ");
-	                    String name = sc.nextLine();
+			System.out.println("\n1. Add Student");
+			System.out.println("2. View All");
+			System.out.println("3. Search Student");
+			System.out.println("4. Remove Student");
+			System.out.println("5. Exit");
 
-	                    System.out.print("Enter Age: ");
-	                    int age = sc.nextInt();
-	                    sc.nextLine();
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				try {
+					System.out.print("Enter ID: ");
+					int id = sc.nextInt();
+					sc.nextLine();
 
-	                    System.out.print("Enter Course: ");
-	                    String course = sc.nextLine();
+					System.out.print("Enter Name: ");
+					String name = sc.nextLine();
 
-	                    manager.addStudent(new Student(id, name, age, course));
-	                    break;
+					System.out.print("Enter Age: ");
+					int age = sc.nextInt();
+					sc.nextLine();
 
-	                case 2:
-	                    manager.displayAll();
-	                    break;
+					System.out.print("Enter Course: ");
+					String course = sc.nextLine();
 
-	                case 3:
-	                    System.out.print("Enter ID: ");
-	                    manager.searchStudent(sc.nextInt());
-	                    break;
+					manager.addStudent(new Student(id, name, age, course));
 
-	                case 4:
-	                    System.out.print("Enter ID: ");
-	                    manager.removeStudent(sc.nextInt());
-	                    break;
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter valid input!");
+				}
+				break;
 
-	                case 5:
-	                	exit = 0;
-	                	sc.close();
-	                	break;
-	                		
-		         
-		         }
-		            
-		            
-		            
+			case 2:
+				manager.displayAll();
+				break;
+
+			case 3:
+				System.out.print("Enter ID: ");
+				try {
+
+					manager.searchStudent(sc.nextInt());
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter valid input!");
+				}
+
+				break;
+
+			case 4:
+				System.out.print("Enter ID: ");
+				try {
+					manager.removeStudent(sc.nextInt());
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter valid input!");
+				}
+
+				break;
+
+			case 5:
+				exit = 0;
+				sc.close();
+				break;
+
 			}
-			
-		
+
+		}
+
 	}
 
 }
